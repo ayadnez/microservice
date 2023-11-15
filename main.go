2038601ef093dea1b78c04fc7837b23d80744987
ayadnez <zahid.ahmad.0303@gmail.com>
2023-11-16 03:02:41 +0530

    added logger

--- main.go
+++ main.go
@@ -3,33 +3,16 @@ package main
 import (
 	"context"
 	"fmt"
+	"log"
 )
 
-type PriceFetcher interface {
-	FetchPrice(context.Context, string) (float64, error)
-}
-
-type priceFetcher struct{}
-
-func (s *priceFetcher) FetchPrice(ctx context.Context, ticker string) (float64, error) {
-	// write business logic here
-	return MockpriceFetcher(ctx, ticker)
-}
-
-var priceMocks = map[string]float64{
-	"BTC": 20_000.0,
-	"ETH": 23_000.0,
-	"GG":  25_0000.0,
-}
-
-func MockpriceFetcher(ctx context.Context, ticker string) (float64, error) {
-	price, ok := priceMocks[ticker]
-	if !ok {
-		return price, fmt.Errorf("the given ticker (%s) is not supported", ticker)
-	}
-	return price, nil
-}
-
 func main() {
-	fmt.Println("hello")
+
+	svc := NewloggingService(&priceFetcher{})
+	price, err := svc.FetchPrice(context.Background(), "ETH")
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(price)
+
 }
